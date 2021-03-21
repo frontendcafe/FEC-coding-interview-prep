9ddfc1e6c8622642696a158f31a5ef9ba3f9c397
AmitSna <18194916+AmitSna@users.noreply.github.com>
2021-03-21 03:28:58 -0300

    Refactor

--- HackerRank/InterviewPreparationKit/py3/Arrays/Left Rotation/leftRotation.AmitSna.py
+++ HackerRank/InterviewPreparationKit/py3/Arrays/Left Rotation/leftRotation.AmitSna.py
@@ -1,7 +1,2 @@
 def rotLeft(a, d):
-    if len(a) >= d:
-        return a[d-len(a):] + a[:d]
-    else:
-        amount = (d % len(a)) + 1
-        new_a = a * amount
-        return a[d-len(a):] + a[:d]
+    return a[d-len(a):] + a[:d]
